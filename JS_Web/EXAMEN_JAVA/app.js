3bff7df0c3bb613e0977324892b897cd6ab23391
ErikaSeven <erika.seven89@gmail.com>
2018-04-17 21:28:17 +0200

    examen bien

--- JS_Web/EXAMEN_JAVA/app.js
+++ JS_Web/EXAMEN_JAVA/app.js
@@ -1,109 +1,113 @@
 import { CURSOS } from './cursos.js'
 
-
 export class Formulario {
     constructor() {
         this.datos = {
-        nombre: '',
-        apellido: '',
-        nacimiento: '',
-        email: '',
-        passwd: '',
-        passwd2: '',
-        datos: '',
-        curso: {},
-        asignaturas: [],
-        dia: '',
-        mes:'',
-        año:'',
-      
-    }
-         
+            nombre: '',
+            apellido: '',
+            nacimiento: '',
+            email: '',
+            passwd: '',
+            datos: '',
+            curso: {},
+            asignaturas: [],
+            passwdR: ''
+
+        }
+
         this.accederDom()
         this.definirManejadores()
         this.ponerFecha()
-
     }
 
- 
-    
- 
- 
     accederDom() {
         this.domFormulario = document.querySelector('#form1')
+        this.domVolar = document.querySelector('#volar')
+        this.domCorrer = document.querySelector('#correr')
+
+        this.domInpNombre = document.querySelector('#nombre')
+        this.domInpApellido = document.querySelector('#apellido')
+        this.domInpFecha = document.querySelector('#fecha')
         this.domInpEmail = document.querySelector('#email')
         this.domInpPasswd = document.querySelector('#passwd')
-        this.domInpPasswd2 = document.querySelector('#passwd2')
-        this.domRadioCurso = document.querySelectorAll('[name="curso"]') 
-        this.domFieldAcademic = document.querySelector('#academic')
+        this.domInpPasswdR = document.querySelector('#passwdR')
         this.domSelectCurso = document.querySelector('#curso')
         this.domSelectTopics = document.querySelector('#topics')
         this.domDivTopics = document.querySelector('#div_topics')
         this.domDivResultados = document.querySelector('#resultados')
         this.donBtnReset = document.querySelector('#reset')
-        this.domInpNombre = document.querySelector('#nombre')
-        this.domInpApellido = document.querySelector('#apellido')
-        this.domInpApellido2 = document.querySelector('#apellido2')
-        this.domInpDia = document.querySelector('#dia')
-        this.domInpMes = document.querySelector('#mes')
-        this.domInpAño = document.querySelector('#año')
-        this.domDivAfi = document.querySelector('#afi')
-        this.domDivAfi2 = document.querySelector('#afi2')
-        this.domTiempo = document.querySelector('#tiempo')
 
+        //this.domFieldAcedemic = document.querySelector('#acedemic')
+        this.domTiempo = document.querySelector('#tiempo')
     }
 
     definirManejadores() {
         this.domFormulario.addEventListener('submit', this.enviar.bind(this))
-        //this.domSelectCurso.addEventListener('change', this.presentarAsignaturas.bind(this))
+        this.domSelectCurso.addEventListener('change', this.presentarAsignaturas.bind(this))
+
     }
 
+ 
 
-    
     enviar(ev) {
         console.log(ev)
         ev.preventDefault()
         this.recogerDatos()
         this.comprobarPasswd()
         this.presentarDatos()
-            
+
+
+
     }
 
     comprobarPasswd() {
         console.log('Estoy pass')
-        if (this.domInpPasswd.value !== this.domInpPasswd2.value) {
+        if (this.domInpPasswd.value !== this.domInpPasswdR.value) {
             alert('Los passwords no coinciden')
             console.log('Estoy aqui')
+
         }
+
     }
+
+    /* borrar () {
+    } */
+    /* 
+        completar() {
+            // this.domFieldAcedemic.disabled = !this.domFieldAcedemic.disabled
+            this.domFieldAcedemic.classList.toggle('ocultar')
+            if (this.domFieldAcedemic.classList.contains('ocultar')) {
+                this.domRadioTurno[0].checked = true
+                this.domSelectCurso.selectedIndex=0 
+            }
+        } */
 
     ponerFecha() {
         let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
         let f = new Date();
-        this.domTiempo.innerHTML = ` Estamos a ${f.getDate()} de ${meses[f.getMonth()]} de ${f.getFullYear()} y es un buen dia para hacer un examen`
+        this.domTiempo.innerHTML = ` Estamos a ${f.getDate()} de ${meses[f.getMonth()]} de ${f.getFullYear()} y es un buen día para hacer un exámen`
+
     }
-
-    completar() {
-        this.domFieldAcademic.classList.toggle('ocultar')
-        if (this.domFieldAcademic.classList.contains('ocultar')) {
-            this.domRadioCurso[0].checked = true
-            this.domSelectCurso.selectedIndex=0 
-        }
-    }
-
-
     recogerDatos() {
-        this.datos.email = this.domInpEmail.value
-        this.datos.passwd =  this.domInpPasswd.value
-        this.datos.curso = this.procesarRadio(this.domRadioCurso)
         this.datos.nombre = this.domInpNombre.value
         this.datos.apellido = this.domInpApellido.value
-        this.datos.apellido = this.domInpApellido2.value
-        this.datos.dia = new Date(this.domInpDia.value)
-        this.datos.mes = new Date(this.domInpMes.value)
-        this.datos.año = new Date(this.domInpAño.value)
-      
+        this.datos.nacimiento = new Date(this.domInpFecha.value)
+        this.datos.email = this.domInpEmail.value
+        this.datos.passwd = this.domInpPasswd.value
+        this.datos.curso = this.procesarSelect(this.domSelectCurso)
     }
+
+    /*  procesarRadio(nodo) {
+         let value
+         nodo.forEach( (item ) => {
+             if (item.checked) {
+                 value = item.value
+             }
+         })
+         return value 
+     } */
+
+     
 
     procesarSelect(nodo) {
         let index = nodo.selectedIndex
@@ -113,64 +117,49 @@ export class Formulario {
         }
     }
 
-    procesarRadio(nodo) {
-    let value
-        nodo.forEach((item) => {
-            if (item.checked) {
-            value = item.value}
-        })
-        return value
+    presentarAsignaturas(ev) {
+        //ev.target.firstElementChild.classList.add('ocultar')
+        //let topics = CURSOS[ev.target.selectedIndex - 1].asignaturas
+        let HTMLResult = ''
+        topics.forEach(elem => HTMLResult += `<option>${elem}</option>`)
+        this.domDivTopics.classList.remove('ocultar')
+        this.domSelectTopics.innerHTML = HTMLResult
     }
-
-
-
-    // presentarAsignaturas(ev) {
-    //     //ev.target.firstElementChild.classList.add('ocultar')
-    //     let topics = CURSOS[ev.target.selectedIndex - 1].asignaturas
-    //     let HTMLResult = ''
-    //     topics.forEach(elem => HTMLResult += `<option>${elem}</option>`)
-    //     this.domDivTopics.classList.remove('ocultar')
-    //     this.domSelectTopics.innerHTML = HTMLResult
-    // }
 
     presentarDatos() {
         let resultadoHTML =
-        `<h2>Resultados</h2>
+            `<h2>Resultados</h2>
         <ul>
-            <li>email: ${this.datos.email}</li>
+            <li>Nombre: ${this.datos.nombre}</li>
+            <li>Apellido: ${this.datos.apellido}</li>
+            <li>Fecha nacimiento: ${this.datos.nacimiento}</li>
+            <li>e-Mail: ${this.datos.email}</li>
             <li>Contraseña: ${this.datos.passwd}</li>
             <li>Asignaturas: ${CURSOS[this.seleccionarIndexCurso()].asignaturas}</li>
             <li>Curso: ${this.datos.curso.text}</li>
-            <li>Nombre: ${this.datos.nombre}</li>
-            <li>Apellido: ${this.datos.apellido}</li>
-            <li>Apellido2: ${this.datos.apellido2}</li>
-            <li>Fecha de nacimiento: ${this.datos.dia}</li>
-            <li>Fecha de nacimiento: ${this.datos.mes}</li>
-            <li>Fecha de nacimiento: ${this.datos.año}</li>
-            <li>Aficiones: ${this.domAfi.value}</li>
-            <li>Aficiones: ${this.domAfi2.value}</li>
+            <li>Aficiones: ${this.domVolar.value}</li>
         </ul>
         `
         this.domDivResultados.innerHTML = resultadoHTML
         console.dir(this.datos)
-         
+
     }
 
-
     seleccionarIndexCurso() {
+        //console.log("estouy dentro")
         let index;
         for (let i = 0; i < CURSOS.length; i++) {
-        if (this.datos.curso.text === CURSOS[i].titulo) {
-            index = i
-            break}
+            //console.log(CURSOS[i].titulo)
+            //console.log(this.datos.curso.text)
+            if (this.datos.curso.text === CURSOS[i].titulo) {
+                index = i
+                break
+            }
+
         }
         console.log(index)
         return index
+
     }
 
-
-  
-
-   
 }
-
